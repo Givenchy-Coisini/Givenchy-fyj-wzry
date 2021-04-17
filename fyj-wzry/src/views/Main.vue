d250e738b6bb1f3b1c9a242e4e790424f9eed27c
mwjfyj999 <2312696405@qq.com>
2021-04-17 12:52:35 +0800

    add router

---
+++ fyj-wzry/src/views/Main.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <!-- 第6个 的 4分钟 -->
+        <div class="topbar"></div>
+        <div class="nav"></div>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
