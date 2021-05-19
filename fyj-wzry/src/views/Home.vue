92ffd8c1ae24ca3e08cb3b68d6e2c53d848d6027
mwjfyj999 <2312696405@qq.com>
2021-05-19 18:31:06 +0800

    首页轮播图完成

--- fyj-wzry/src/views/Home.vue
+++ fyj-wzry/src/views/Home.vue
@@ -1,16 +1,46 @@
 <template>
   <div>
-    <swiper>
-      <swiper-slide>111</swiper-slide>
-      <swiper-slide>222</swiper-slide>
-      <swiper-slide>333</swiper-slide>
+    <swiper :options="swiperOptions">
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/819841077459195.jpg" alt="">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/819841077459195.jpg" alt="">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/819841077459195.jpg" alt="">
+      </swiper-slide>
+      <div class="swiper-pagination pagination-home text-right px-3" slot="pagination"></div>
     </swiper>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      swiperOptions: {
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        autoplay: true
+        // Some Swiper option/callback...
+      }
+    }
+  }
+}
 </script>
 
 <style lang="scss">
+@import '../assets/scss/style.scss';
+.pagination-home {
+  .swiper-pagination-bullet {
+    opacity: 1;
+    border-radius: .153846rem ;
+    background: #fff;
+    &.swiper-pagination-bullet-active {
+      background: map-get($colors, 'info');
+    }
+  }
+}
 </style>
