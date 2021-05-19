6ea7e39bbab53ada7ed6a9abcf66aac50a8b62a0
mwjfyj999 <2312696405@qq.com>
2021-05-19 13:33:14 +0800

    轮播图样式完成

--- fyj-wzry/src/main.js
+++ fyj-wzry/src/main.js
@@ -3,10 +3,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
-import 'swiper/dist/css/swiper.css'
+import 'swiper/css/swiper.css'
 
 Vue.config.productionTip = false
-Vue.use(VueAwesomeSwiper, / * { default options with global component } */)
+Vue.use(VueAwesomeSwiper)
 new Vue({
   router,
   render: h => h(App)
